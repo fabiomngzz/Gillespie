b30877479500a72b81c94d617d4f4ea2c93a7ecb
fabiomngzz <fabio.mngzz@gmail.com>
2025-08-24 18:43:12 +0200

    Check for absorbing states

--- Gillespie.py
+++ Gillespie.py
@@ -1,15 +1,30 @@
 import numpy as np
 from scipy.interpolate import PchipInterpolator
-from helpers import evalContextVar
+from helpers import evalContextVar, speciesFrac
+from CPRSust_metapop_rates import consumersEncounterFactor
 
 def GillespieStep(context,reactions):
     vec = evalContextVar(['varVec'],context)[0]
     # Choose a process according to their propensities
     reactsRates = [r['probFunc'](*evalContextVar(r['probFuncVars'],context)) for r in reactions]
+    # Remove the null rates
+    positiveReactsRates = [r for r in reactsRates if r > 0]
     #print('\t\t',reactsRates)
+    # Check for absorbing state
+    absState = False
+    if len(positiveReactsRates)==0:
+        absState = True
+        #print(absState)
+        # If the absorbing state is reached, exit
+        return absState, {
+            't' : 0,
+            'vec' : vec
+        }
+
     ratesCum = np.cumsum(reactsRates)
     probsCum = ratesCum / ratesCum[-1]
-    #print('\t\t',ratesCum)
+    #print('\t\t',probsCum)
+
     while True:
         temp = np.random.rand()
         for ii in range(len(reactions)):
@@ -23,17 +38,9 @@ def GillespieStep(context,reactions):
                 # Check if there are candidates for the update; if so, perform the update
                 potentialIdxs = [np.where(vec == s)[0] for s in oldState]
                 #print('\t\t\tCandidates for update: ',len(potentialIdxs))
-                # Check if there are neighbors of the required state
-                requiredNeighborState = evalContextVar(reactionCurrent['neighborState'],context)
-                # print('\t\t\t',requiredNeighborState)
-                if requiredNeighborState:
-                    checkNeighborState = np.any([np.where(vec == s)[0] for s in requiredNeighborState])
-                else:
-                    checkNeighborState = True
-                #print('\t\t\t',checkNeighborState)
                 nPotentialUpdates = np.array([len(pI) for pI in potentialIdxs])
                 #print('\t\t\t',nPotentialUpdates)
-                if np.all(nPotentialUpdates > 0) and checkNeighborState:
+                if np.all(nPotentialUpdates > 0):
                     #print('\t\t\tRandom number ',temp)
                     #print('\t\t\t',reactionCurrent['description'])
                     
@@ -48,21 +55,30 @@ def GillespieStep(context,reactions):
 
                     # Update time
                     tStep = np.log(1/temp)/(ratesCum[-1])
-                    return {
+                    return absState, {
                         't' : tStep,
                         'vec' : vec
                     }
                 break
 
-def tVecCommon(tVec,context,reactions,mulF=1):
-    tMaxGill = min(max(t) for t in tVec)
+def tVecCommon(seriesObj,context,reactions,timeLabel='time',absorbingLabel='absorbing',mulF=1):
+    tVecNonAbs = [obj[timeLabel] for obj in seriesObj if not obj[absorbingLabel]]
+    if len(tVecNonAbs)>0:
+        tMaxGill = min(max(t) for t in tVecNonAbs)
+    else:
+        tMaxGill = max(obj[timeLabel][-1] for obj in seriesObj)
     ratesTyp = np.sum([r['probFunc'](*evalContextVar(r['probFuncVars'],context)) for r in reactions])
     timeScaleTyp = mulF*ratesTyp**-1
     return np.arange(0,tMaxGill,timeScaleTyp)
 
-def GillTimeSeriesInterp(tAvg,tVec,dataVec):
+def GillTimeSeriesInterp(tVecAvg,seriesObj,dataLabel,timeLabel='time',absorbingLabel='absorbing'):
     dataInterpVec = []
-    for t, data in zip(tVec,dataVec):
-        dataInterp = PchipInterpolator(t,data)
-        dataInterpVec.append(dataInterp(tAvg))
+    for obj in seriesObj:
+        dataInterpObj = PchipInterpolator(obj[timeLabel],obj[dataLabel],extrapolate=False)
+        dataInterp = dataInterpObj(tVecAvg)
+        if obj[absorbingLabel]:
+            tMax = obj[timeLabel][-1]
+            finalState = obj[dataLabel][-1]
+            dataInterp[tVecAvg > tMax] = finalState
+        dataInterpVec.append(dataInterp)
     return dataInterpVec
