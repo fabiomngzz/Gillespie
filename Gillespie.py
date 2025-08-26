28d799d6be00dfcc443c84f5f5a360fbb3d71524
fabiomngzz <fabio.mngzz@gmail.com>
2025-08-26 18:39:44 +0200

    Added code for evaluation of times to extinction

--- Gillespie.py
+++ Gillespie.py
@@ -81,3 +81,36 @@ def GillTimeSeriesInterp(tVecAvg,seriesObj,dataLabel,timeLabel='time',absorbingL
             dataInterp[tVecAvg > tMax] = finalState
         dataInterpVec.append(dataInterp)
     return dataInterpVec
+
+# def extinctionTime_single(t,y,tol=1e-3):
+#     # Get all the indexes for which the resource is 0
+#     idxExtVec = np.where(np.array(y) <= tol)[0]
+#     if len(idxExtVec)==0:
+#         return None
+#     idxExt = idxExtVec[0]
+#     # Perform a simple linear interpolation to better estimate the extinction time
+#     tExt_temp = [t[idxExt-1],t[idxExt]]
+#     yExt_temp = [y[idxExt-1],y[idxExt]]
+#     return tExt_temp[0] + yExt_temp[0]*(tExt_temp[1]-tExt_temp[0])/(yExt_temp[0]-yExt_temp[1])
+
+# def extinctionTimes(seriesObj,timeLabel='time',resLabel='resource',tol=1e-3):
+#     # Calculate the extinction times for each time series
+#     tExtVec = []
+#     for obj in seriesObj:
+#         tExt = extinctionTime_single(obj[timeLabel],obj[resLabel],tol)
+#         if tExt is not None:
+#             tExtVec.append(tExt)
+    
+#     if tExtVec:
+#         tExtAvg = np.mean(tExtVec)
+#         tExtStd = np.std(tExtVec)
+#     else:
+#         tExtAvg = None
+#         tExtStd = None
+    
+#     return {
+#         'times' : tExtVec,
+#         'mean' : tExtAvg,
+#         'std' : tExtStd,
+#         'fraction' : len(tExtVec)/len(seriesObj)
+#     }
--- helpers.py
+++ helpers.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def setVec(vec,val):
     vec[:]=val
@@ -19,27 +18,6 @@ def countSpecies(vec,targetState):
 
 def speciesFrac(vec,targetState):
     return len(getSubvec(vec,targetState))/len(vec)
-
-def makePlots(plotsObj,tRep=False):
-    if tRep:
-        NRep = np.shape(plotsObj[1]['series'])[0]
-        tVec = [plotsObj[0]['series'][k] for k in range(len(NRep))]
-    else:
-        tVec = plotsObj[0]['series']
-
-    fig, axes = plt.subplots(1,2,figsize=(16,8))
-
-    for ax,t,plotObj in zip(axes,tVec,plotsObj[1:]):
-        ax.set_title(plotObj['name'])
-        ax.plot(t,plotObj['series'],plotObj['color'], alpha=0.4)
-        ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-        ax.set_xlabel(plotObj['xlabel'])
-        ax.set_ylabel(plotObj['ylabel'])
-        ax.set_ylim(plotObj['yLims'])
-        if plotObj['computeMean']:
-            ax.hlines(np.mean(plotObj['series']),0,t[-1],linestyles='dashed',color=plotObj['color'],alpha=1)
-
-    return fig, axes
 
 def evalContextVar(names, context):
     return [eval(name, {}, context) for name in names]
